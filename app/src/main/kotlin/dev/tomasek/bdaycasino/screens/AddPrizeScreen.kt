509ba2296ce9445125acd07f7763bfcb988b1e6c
Martin Tomášek <martin@tomasek.dev>
2025-04-02 21:25:03 +0200

    Add dialog for adding prizes and update UI for prize list

--- app/src/main/kotlin/dev/tomasek/bdaycasino/screens/AddPrizeScreen.kt
+++ app/src/main/kotlin/dev/tomasek/bdaycasino/screens/AddPrizeScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
